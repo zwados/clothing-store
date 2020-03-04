582e3256c0e8bd910021d28e108ba74f93de2261
zwados <lukasz.rozwadowski7@gmail.com>
2020-03-04 19:13:22 +0100

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
@@ -6,7 +7,9 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,28 +12,33 @@ class Directory extends Component {
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
+                linkUrl: 'hats',
             },
             {
                 title: 'jackets',
                 imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                 id: 2,
+                linkUrl: 'jackets',
             },
             {
                 title: 'sneakers',
                 imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                 id: 3,
+                linkUrl: 'sneakers',
             },
             {
                 title: 'womens',
                 imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                 id: 4,
                 size: 'large',
+                linkUrl: 'womens',
             },
             {
                 title: 'mens',
                 imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                 id: 5,
                 size: 'large',
+                linkUrl: 'mens',
             }
         ]
          }
@@ -41,7 +46,7 @@ class Directory extends Component {
 
     render() { 
         return ( 
-        <div className='directory-menu'>{this.state.sections.map(({title, id, imageUrl, size})=> <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>)}</div> );
+        <div className='directory-menu'>{this.state.sections.map(({id, ...otherSectionProps})=> <MenuItem key={id} {...otherSectionProps}/>)}</div> );
     }
 }
  
