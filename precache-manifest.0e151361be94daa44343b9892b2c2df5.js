72456c07b26d2254e381405842bcc4a8b97305b2
zwados <lukasz.rozwadowski7@gmail.com>
2020-03-18 21:44:31 +0100

    Updates

---
+++ precache-manifest.0e151361be94daa44343b9892b2c2df5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca85e45d64e9b8b7e9df947cd435d3a0",
+    "url": "/clothing-store/index.html"
+  },
+  {
+    "revision": "2243c12a967baf7b1754",
+    "url": "/clothing-store/static/css/main.e467ec3a.chunk.css"
+  },
+  {
+    "revision": "6f89ab35a9a0c8b08ba5",
+    "url": "/clothing-store/static/js/2.6bae88f4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/clothing-store/static/js/2.6bae88f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2243c12a967baf7b1754",
+    "url": "/clothing-store/static/js/main.4505b0d5.chunk.js"
+  },
+  {
+    "revision": "2e5cb6936444ea6a21c1",
+    "url": "/clothing-store/static/js/runtime-main.981e2303.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing-store/static/media/logo.ed28accb.svg"
+  }
+]);
